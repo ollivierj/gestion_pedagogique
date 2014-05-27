d4ddc334f5459e1882530ef89a3eb598747e02ce
Fabien <Fabien>
2014-05-27 20:14:52 +0200

    Ajout gestion droit

--- GST_PDG_MAQUETTE/app/js/app.js
+++ GST_PDG_MAQUETTE/app/js/app.js
@@ -69,6 +69,10 @@ angular.module('ng_gst_pdg', ['ngRoute', 'ngGrid', 'ui.bootstrap','ui.tree','ng_
 			templateUrl: 'partials/jury/consultationJury.html',
 			controller: 'consultationJuryCtrl'
 		}).
+		when('/gestionDroit', {
+			templateUrl: 'partials/droit/gestionDroit.html',
+			controller: 'gestionDroitCtrl'
+		}).
 		otherwise({
 			redirectTo: '/accueil'
 		});
