5e01464df766e6750ad0efddb47822eae0ee4e2a
jollivier <ollivierj@gmail.com>
2014-07-04 13:12:00 +0200

    a

--- GST_PDG_APPLICATION/src/main/webapp/js/controllers/professionnelHomologueControllers.js
+++ GST_PDG_APPLICATION/src/main/webapp/js/controllers/professionnelHomologueControllers.js
@@ -61,17 +61,48 @@ controllers
 					};
 
 					$scope.editRow = function(professionnelHomologue) {
-						$scope
-								.editerProfessionnelHomologue(professionnelHomologue.id);
+						$scope.editerProfessionnelHomologue(professionnelHomologue.id);
 					};
 
 					$scope.viewRow = function(professionnelHomologue) {
-						$scope
-								.editerProfessionnelHomologue(professionnelHomologue.id);
+						$scope.editerProfessionnelHomologue(professionnelHomologue.id);
 					};
 					
 					$scope.removeRow = function(professionnelHomologue) {
 						$scope.supprimerProfessionnelHomologue(professionnelHomologue.id);
+					};
+					
+					$scope.afficherFenetreEdition = function(){
+						$scope.ajouterProfessionnelHomologue();
+					}
+					
+					$scope.ajouterProfessionnelHomologue = function(
+							professionnelHomologueId) {
+						var modalAdd = $modal
+								.open({
+									templateUrl : 'partials/professionnelHomologues/professionnelHomologuesFormulaire.html',
+									controller : modalEditionProfessionnelHomologueCtrl,
+									resolve : {
+										professionnelHomologue : function(){ return {}},
+										ok : function() { return function(){
+											ProfessionnelHomologuesFactory.create
+													.doAction($scope.professionnelHomologue).$promise.then(
+													function(response) {
+														$modalInstance.close($scope.professionnelHomologue);
+													}, function(reason) {
+														alert('Failed: ' + reason);
+													});
+										}
+										}
+										
+									}
+								});
+
+						modalAdd.result.then(function(selectedItem) {
+							ProfessionnelHomologuesFactory.refreshData($scope);
+						}, function() {
+							$log.info('Modal dismissed at: ' + new Date());
+						});
 					};
 
 					$scope.editerProfessionnelHomologue = function(
@@ -88,10 +119,15 @@ controllers
 														id : professionnelHomologueId
 													}).$promise;
 										},
-										homologations : function(
-												homologationData) {
-											return homologationData.getAll()
-													.query().$promise;
+										ok : function() { return function(){
+											ProfessionnelHomologuesFactory.modify
+													.doAction($scope.professionnelHomologue).$promise.then(
+													function(response) {
+														$modalInstance.close($scope.professionnelHomologue);
+													}, function(reason) {
+														alert('Failed: ' + reason);
+													});
+											}
 										}
 									}
 								});
@@ -112,7 +148,21 @@ controllers
 									resolve : {
 										id : function() {return professionnelHomologueId},
 										title : function() {return "Suppression professionnel homologué"},
-										message : function() {return "Etes-vous sur de vouloir supprimer ce professionnel homologué ?"}
+										message : function() {return "Etes-vous sur de vouloir supprimer ce professionnel homologué ?"},
+										ok : function () { return function() {
+											ProfessionnelHomologuesFactory.delete.doAction(
+													{
+													id : $scope.id
+													}).$promise.then(
+															function(response) {
+																$modalInstance.close($scope.professionnelHomologue);
+															}, 
+															function(reason) {
+																alert('Failed: ' + reason);
+															}
+													);
+											};
+										}
 										
 									}
 								});
@@ -127,43 +177,20 @@ controllers
 				});
 
 var modalEditionProfessionnelHomologueCtrl = function($scope, $modalInstance,
-		ProfessionnelHomologuesFactory, professionnelHomologue, homologations) {
+		ProfessionnelHomologuesFactory, professionnelHomologue, ok) {
 	$scope.professionnelHomologue = professionnelHomologue;
-	$scope.homologations = homologations;
-
-	$scope.ok = function() {
-		ProfessionnelHomologuesFactory.modify
-				.doAction($scope.professionnelHomologue).$promise.then(
-				function(response) {
-					$modalInstance.close($scope.professionnelHomologue);
-				}, function(reason) {
-					alert('Failed: ' + reason);
-				});
-	};
-
+	$scope.ok = ok ;
 	$scope.cancel = function() {
 		$modalInstance.dismiss('cancel');
 	};
 };
 
 var modalConfirmationDeleteProfessionnelHomologueCtrl = function($scope,
-		$modalInstance, ProfessionnelHomologuesFactory, id, title, message) {
+		$modalInstance, ProfessionnelHomologuesFactory, id, title, message, ok) {
 	$scope.id = id;
 	$scope.title = title;
 	$scope.message = message;
-	
-	$scope.ok = function() {
-		ProfessionnelHomologuesFactory.delete
-		.doAction({
-			id : $scope.id
-		}).$promise.then(
-		function(response) {
-			$modalInstance.close($scope.professionnelHomologue);
-		}, function(reason) {
-			alert('Failed: ' + reason);
-		});
-	};
-
+	$scope.ok = ok;
 	$scope.cancel = function() {
 		$modalInstance.dismiss('cancel');
 	};
