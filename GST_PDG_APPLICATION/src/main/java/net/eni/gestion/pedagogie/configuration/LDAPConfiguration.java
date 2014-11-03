1169937a8c5a3519491473c28799e4df1ee69712
fgirardeau <fabien.girardeau@gmail.com>
2014-11-03 17:34:51 +0100

    modification fichier configuration.properties ajout ldap + bdd

--- GST_PDG_APPLICATION/src/main/java/net/eni/gestion/pedagogie/configuration/LDAPConfiguration.java
+++ GST_PDG_APPLICATION/src/main/java/net/eni/gestion/pedagogie/configuration/LDAPConfiguration.java
@@ -3,6 +3,8 @@
  */
 package net.eni.gestion.pedagogie.configuration;
 
+import net.eni.gestion.pedagogie.commun.composant.PropertyFileLoader;
+
 
 /**
  * @author fgirardeau
@@ -10,22 +12,58 @@ package net.eni.gestion.pedagogie.configuration;
  */
 public class LDAPConfiguration {
 	
+	public final static PropertyFileLoader propertyFileLoader = PropertyFileLoader
+			.getInstance("configuration");
+	
 	/** Nom du domaine du serveur ldap */
-	public final static String LDAP_DOMAINE = "eni.local";
+	public final static String LDAP_DOMAINE_DEFAULT = "eni.local";
 	
 	
 	/** IP du serveur ldap */
-	public final static String LDAP_IP = "192.168.0.48";
+	public final static String LDAP_IP_DEFAULT = "192.168.0.48";
 
 	/** Port de connection au serveur ldap */
-	public final static String LDAP_PORT = "389";
+	public final static String LDAP_PORT_DEFAULT = "389";
 
 	/** Lors de la création d'un utilisateur depuis LDAP numero du profil par default a lui affecter */
 	public final static Integer LDAP_CREATE_USER_DEFAULT_PROFIL = 1;
 	
 	
+	
+	public static String getLdapDomaine(){
+		String value = propertyFileLoader.getValue("ldap.domaine");
+		if(value == null || value.trim().equals("")){
+			return LDAP_DOMAINE_DEFAULT;
+		}
+		return value;
+	}
+	
+	public static String getLdapIp(){
+		String value = propertyFileLoader.getValue("ldap.ip");
+		if(value == null || value.trim().equals("")){
+			return LDAP_IP_DEFAULT;
+		}
+		return value;
+	}
+	
+	public static String getLdapPort(){
+		String value = propertyFileLoader.getValue("ldap.port");
+		if(value == null || value.trim().equals("")){
+			return LDAP_PORT_DEFAULT;
+		}
+		return value;
+	}
+	
+	public static Integer getDefaultUserProfil(){
+		String value = propertyFileLoader.getValue("ldap.user.default.profil");
+		if(value == null || value.trim().equals("")){
+			return LDAP_CREATE_USER_DEFAULT_PROFIL;
+		}
+		return Integer.parseInt(value);
+	}
+	
 	public static String getAdresseLDAP(){
-		return LDAP_IP+":"+LDAP_PORT;
+		return getLdapIp()+":"+getLdapPort();
 	}
 	
 }
